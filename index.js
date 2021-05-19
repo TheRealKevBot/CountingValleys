239d32bafbabd8250f0a6be5a7df1c9ab83f4a73
TheRealKevBot <Kglas001@gmail.com>
2021-05-19 15:34:26 -0600

    First

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ * https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ */
+
+let example = {
+    steps: 8,
+    path: [U, D, D, D, U, D, U, U]
+} 
+
+function countingValleys(steps, path) {
+    // Write your code here
+
+} 
+
+// countingValleys(example.steps, example.path) 
+
+// console.log(countingValleys(example.steps, example.path))
